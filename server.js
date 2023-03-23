5d94fcd7b05fae6067a03039675eb2f10ce3b805
AlfaroDeveloper <49664287+AlfaroDeveloper@users.noreply.github.com>
2023-03-22 23:59:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+
 const axios = require("axios");
 const cheerio = require("cheerio");
 const { engine } = require("express-handlebars");
@@ -40,7 +40,7 @@ const hbs = Handlebars.create({
 });
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static("images"));
 
@@ -357,7 +357,7 @@ function arrayLength(array, iteration) {
 }
 
 app.get("/", (req, res) => {
-  req.header(false);
+  
   res.header("Access-Control-Allow-Origin", "*");
   res.render("home");
 });
@@ -365,7 +365,7 @@ app.get("/", (req, res) => {
 app.get("/bibleStudy", (req, res) => {
   let imageList = [];
   imageList.push({ src: "/images/icon.png", name: "logo" });
-  req.header(false);
+  
   res.header("Access-Control-Allow-Origin", "*");
   res.render("index", {
     lastTyped,
@@ -380,7 +380,7 @@ app.get("/bibleStudy", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-  req.header(false);
+  
   res.json({
     lastTyped,
     commentOne,
@@ -525,7 +525,7 @@ app.get("/verse/:book/:chapter/:verse", (req, res) => {
       );
     })
     .catch((err) => console.log(err));
-  req.header(false);
+  
   res.header("Access-Control-Allow-Origin", "*");
 });
 
