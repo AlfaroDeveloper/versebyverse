79b9fd74ba34c34b5ef620d87418cc23a5697486
AlfaroDeveloper <49664287+AlfaroDeveloper@users.noreply.github.com>
2023-03-23 01:39:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,48 +3,15 @@ const cors = require("cors");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const { engine } = require("express-handlebars");
-const Handlebars = require("express-handlebars");
 const app = express();
-const hbs = Handlebars.create({
-  defaultLayout: "main",
 
-  //   helper
-  helpers: {
-    when: function (operand_1, operator, operand_2, options) {
-      var operators = {
-          eq: function (l, r) {
-            return l == r;
-          },
-          noteq: function (l, r) {
-            return l != r;
-          },
-          gt: function (l, r) {
-            return Number(l) > Number(r);
-          },
-          or: function (l, r) {
-            return l || r;
-          },
-          and: function (l, r) {
-            return l && r;
-          },
-          "%": function (l, r) {
-            return l % r === 0;
-          },
-        },
-        result = operators[operator](operand_1, operand_2);
-
-      if (result) return options.fn(this);
-      else return options.inverse(this);
-    },
-  },
-});
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static("images"));
 
-app.engine("handlebars", hbs.engine);
+app.engine("handlebars", engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
